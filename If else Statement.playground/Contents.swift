4acca8e52f3812a95076fdf130a466acaac44e90
Student13 <weerapatit122@gmail.com>
2017-06-13 15:14:38 +0700

    If

---
+++ If else Statement.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var intAge = 45
+
+
+if intAge > 50 {
+    print(" You are Ole ")
+}else{
+    print(" You are Young ")
+}
+
+
+
