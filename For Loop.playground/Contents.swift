6d05aab7f7462e56cbe0fe949a43b3f15f399e83
Student13 <weerapatit122@gmail.com>
2017-06-14 13:40:38 +0700

    For Loop

---
+++ For Loop.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Implcit
+let intNumber = [11,22,33,44,55]
+var intIndex = 0
+
+
+for intArray in intNumber {
+    print("intArray ==> (\(intIndex) ) \(intArray)")
+    intIndex += 1
+}
+
+//For Loop  ใช้กับ String
+let strArrayName = ["AAA","BBB","CCC","DDD"]
+for strMyName in strArrayName {
+    
+    print("StrMyName ==> \(strMyName)")
+    
+}
+		
